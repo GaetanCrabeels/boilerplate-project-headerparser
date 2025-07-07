fdea6d1e675b89a534a9f9adbd80fbff13449a16
gaetancrabeels@hotmail.com <Crabeels.gaetan@outlook.be>
2025-07-07 15:09:41 +0200

    Who I am (IP/Language/Software

--- index.js
+++ index.js
@@ -18,10 +18,12 @@ app.use(express.static('public'));
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
-
-// your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', function (req, res) {
+  res.json({
+    ipaddress: req.ip,
+    language: req.headers['accept-language'],
+    software: req.headers['user-agent']
+  });
 });
 
 // listen for requests :)
